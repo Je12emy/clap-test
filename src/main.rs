7643b65b542169c8d0790fbe800f925dcd9bfb11
Jeremy Zelaya <jzelaya@krames.com>
2023-07-30 22:50:32 -0600

    feature: add global flags

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, CommandFactory, Parser, Subcommand, error::ErrorKind};
+
+pub const INVALID_TOKEN_DEFAULT_VALUE: &str = "INVALID";
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 struct Cli {
     #[clap(subcommand)]
     resource: Resource,
+    // Enable verbose logging
+    #[arg(default_value = "true", global = true, short, long)]
+    verbose: bool,
+    /// Personal access token
+    #[arg(global = true, default_value = INVALID_TOKEN_DEFAULT_VALUE, short, long)]
+    pub access_token: String,
 }
 
 #[derive(Debug, Subcommand)]
@@ -23,19 +31,22 @@ enum UserCommand {
 #[derive(Debug, Args)]
 struct CreateUserCommand {
     /// The username
+    #[arg(short, long)]
     username: String,
     /// The email
+    #[arg(short, long)]
     email: String,
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("{:?}", args);
-    match args.resource {
-        Resource::User(user) => match user {
-            UserCommand::Create(user_create) => {
-                println!("{:?}", user_create)
-            }
-        },
+    let mut cmd = Cli::command();
+    if args.access_token.eq(INVALID_TOKEN_DEFAULT_VALUE) {
+        cmd.error(
+            ErrorKind::InvalidValue,
+            "Please provide a valid value for access token",
+        )
+        .exit();
     }
+    println!("{:?}", args);
 }
